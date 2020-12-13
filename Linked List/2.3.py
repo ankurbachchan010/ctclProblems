2f7fe8852060ab2473cfbbb738d61dcb087ddca7
Ankur Bachchan <ankur@steinnlabs.com>
2020-12-13 11:19:17 +0000

    final update

--- Linked List/2.3.py
+++ Linked List/2.3.py
@@ -22,6 +22,21 @@ class Linkedlist:
             elements.append(current_node.data)
         return elements
 
+    def delete_middle(self, node):
+        if self.head.next.data == node.data or self.tail.data == node.data:
+            return False
+        else:
+            current_node = self.head
+            last_node = current_node
+            current_node = current_node.next
+            while True:
+                if current_node.data == node.data:
+                    last_node.next = current_node.next
+                    break
+                last_node = current_node
+                current_node = current_node.next
+            return True
+
 
 n = Linkedlist()
 n.insert(1)
@@ -29,3 +44,9 @@ n.insert(2)
 n.insert(3)
 n.insert(4)
 print(n.display())
+node = Node(3)
+print(n.delete_middle(node))
+print(n.display())
+
+
+
